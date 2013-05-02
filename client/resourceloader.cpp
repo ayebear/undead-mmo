9d7a760cc9a5b21140c056b12f50712be663450f
undisclosed1 <undisclosed.colleague@gmail.com>
2013-05-02 17:25:03 -0400

    resource loader broken

---
+++ client/resourceloader.cpp
@@ -0,0 +1,17 @@
+#include "resourceloader.h"
+
+ResourceLoader::ResourceLoader()
+{
+    //ctor
+}
+
+void LoadTiles()
+{
+    for ()
+    {
+
+    }
+    if (!texture.loadFromFile("data/images/tiles/tiles.png"))
+        exit(Errors::graphics)
+     texturestexture setSmooth(true);
+}
---
+++ client/resourceloader.h
@@ -0,0 +1,21 @@
+#ifndef RESOURCELOADER_H
+#define RESOURCELOADER_H
+
+
+class ResourceLoader
+{
+    public:
+        ResourceLoader();
+        void LoadFiles();
+    private:
+        void LoadFonts();
+        void LoadImages();
+        void LoadSound();
+        void LoadTiles();
+        sf::Font defaultFontMono;
+        sf::Font defaultFontBold;
+        static vector<sf::Texture> textures;
+
+};
+
+#endif // RESOURCELOADER_H
