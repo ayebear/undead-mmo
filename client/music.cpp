ad9061e1d996b6b962ba375af8d41ac867be34f3
undisclosed1 <undisclosed.colleague@gmail.com>
2013-11-14 15:13:12 -0500

    dded audio class and demod it in mainmenustate. still needs to change on state changes and be optimized in memory.

---
+++ client/music.cpp
@@ -0,0 +1,23 @@
+#include "music.h"
+
+music::music(std::vector<std::string> songList)
+{
+    currentSong.openFromFile(songList[0]);
+    currentSong.setLoop(true);
+    currentSong.play();
+}
+
+void music::startMusic()
+{
+    currentSong.play();
+}
+
+void music::stopMusic()
+{
+    currentSong.stop();
+}
+
+void music::setVolume(float vol)
+{
+    currentSong.setVolume(vol);
+}
---
+++ client/music.h
@@ -0,0 +1,23 @@
+#ifndef MUSIC_H
+#define MUSIC_H
+
+#include <string>
+#include <SFML/Audio.hpp>
+
+const std::vector<std::string> menuSongs = {"data/audio/music/titleScreen.ogg"};
+
+class music
+{
+    public:
+        music(std::vector<std::string> songList);
+
+        void startMusic();
+        void stopMusic();
+        void setVolume(float);
+    protected:
+    private:
+        float volume = 70;
+        sf::Music currentSong;
+};
+
+#endif // MUSIC_H
