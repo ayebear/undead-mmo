eee9f00cf804c879555e4f4d4deb2601a897ef67
calcfreak89 <calcfreak89@gmail.com>
2013-11-28 00:51:57 -0500

    Redesigned ObjectAllocator Note: Read comments about the erase function Updated the Windows Codeblocks project

--- other/objectallocator.h
+++ other/objectallocator.h
@@ -5,108 +5,79 @@
 #define OBJECTALLOCATOR_H
 
 #include <vector>
-#include <queue>
 
-using std::size_t;
-
-/*
-This class manages a vector of objects in an efficient way to reduce the amount of memory allocations.
-TODO:
-    Look into a more efficient container than a vector for resizing, we don't want it to copy all of the elements whenever it resizes.
-    size_t is being used as the ID type, maybe make this a typedef in the class (will have to use typename or something as well)
-    Figure out ways to make this more robust, such as using a parallel bool array (or 2 queues) for determining which objects are allocated/unused.
-*/
+/// This class manages a vector of objects in an efficient way to reduce the amount of memory allocations.
 template <class Type>
 class ObjectAllocator
 {
     public:
-        ObjectAllocator(size_t = 32);
-        ~ObjectAllocator();
 
-        //size_t allocate(const Type&); // Will we even need this?
-        size_t allocate(); // Returns an ID to an unused object
-        bool find(size_t) const; // Returns true if this ID has an object already allocated
-        Type& operator[] (size_t); // Returns a reference to the object with the specfied ID
-        void erase(size_t); // "Deallocates" an object
+        ObjectAllocator()
+        {
+        }
+
+        // Pre-allocate an amount of elements
+        ObjectAllocator(unsigned elementsToReserve)
+        {
+            elements.reserve(elementsToReserve);
+        }
+
+        // Allocates space for a new object and returns its ID
+        int allocate()
+        {
+            elements.resize(elements.size() + 1);
+            return (elements.size() - 1);
+        }
+
+        // Constructs a new object and returns its ID
+        int allocate(const Type& obj)
+        {
+            elements.push_back(obj);
+            return (elements.size() - 1);
+        }
+
+        // Returns true if this ID has an object already allocated
+        bool find(int id) const
+        {
+            return (id < elements.size());
+        }
+
+        // Returns a reference to the object with the specfied ID
+        Type& operator[] (int id)
+        {
+            return elements[id];
+        }
+
+        // "Deallocates" an object by swapping, returns the ID of the swapped element
+        int erase(int id)
+        {
+            // This function will replace the erased object with the last one in the vector.
+            // Therefore it is important to update the ID of the object that was moved (the ID is returned).
+            int oldId = -1; // Returns -1 if nothing is erased or if no action is required
+            if (find(id))
+            {
+                if (elements.size() > 0)
+                {
+                    elements[id] = elements.back(); // Replace the old object with the last one
+                    elements.pop_back(); // Remove the last object
+                    oldId = elements.size(); // The ID of the old last object, which now has the ID of the erased element
+                }
+                else if (elements.size() == 1)
+                    elements.pop_back(); // Remove the last and only object
+            }
+            return oldId;
+        }
+
+        // Returns the current size
+        int size()
+        {
+            return elements.size();
+        }
 
     private:
-        bool validId(size_t) const;
-        void preallocate(size_t);
 
-        size_t chunkSize;
         std::vector<Type> elements;
-        std::queue<size_t> freeIds;
+
 };
-
-template <class Type>
-ObjectAllocator<Type>::ObjectAllocator(size_t chunks):
-    chunkSize(chunks)
-{
-}
-
-template <class Type>
-ObjectAllocator<Type>::~ObjectAllocator()
-{
-}
-
-template <class Type>
-size_t ObjectAllocator<Type>::allocate()
-{
-    size_t id = -1;
-    if (freeIds.empty())
-        preallocate(chunkSize);
-    id = freeIds.front();
-    freeIds.pop();
-    /*
-    if (freeIds.empty())
-    {
-        id = elements.size();
-        //elements.push_back(value);
-        preallocate(chunkSize);
-    }
-    else
-    {
-        id = freeIds.front();
-        freeIds.pop();
-        //elements[id] = value;
-    }
-    */
-    return id;
-}
-
-template <class Type>
-bool ObjectAllocator<Type>::find(size_t id) const
-{
-    // Can't properly determine this unless we keep track of the used IDs
-    return validId(id);
-}
-
-template <class Type>
-Type& ObjectAllocator<Type>::operator[] (size_t id)
-{
-    return elements[id];
-}
-
-template <class Type>
-void ObjectAllocator<Type>::erase(size_t id)
-{
-    if (validId(id))
-        freeIds.push(id);
-}
-
-template <class Type>
-bool ObjectAllocator<Type>::validId(size_t id) const
-{
-    return (id < elements.size());
-}
-
-template <class Type>
-void ObjectAllocator<Type>::preallocate(size_t chunks)
-{
-    size_t oldSize = elements.size();
-    elements.resize(oldSize + chunks);
-    for (size_t i = oldSize; i < elements.size(); ++i)
-        freeIds.push(i);
-}
 
 #endif
